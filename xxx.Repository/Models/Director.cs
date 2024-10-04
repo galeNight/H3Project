0461486a875f6add58428656f6e0e27a5ed5bcb0
Jannick Mørup <jannickmorup@gmail.com>
2024-10-04 12:05:28 +0200

    ..

--- xxx.Repository/Models/Director.cs
+++ xxx.Repository/Models/Director.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using xxx.Repository.Interfaces;
 
 namespace xxx.Repository.Models
@@ -12,6 +13,7 @@ namespace xxx.Repository.Models
         public string Name { get; set; }
 
         // En instruktør kan have mange film
+        [JsonIgnore]
         public ICollection<Movie>? Movies { get; set; }
     }
 }
