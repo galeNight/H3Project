aafd27b9be37f3f523d1d419dc9b8be9f47d7b94
galeNight <jannickmorup@gmail.com>
2024-10-23 08:07:18 +0200

    ...

--- xxx.Repository/Migrations/20241022080135_initial.cs
+++ xxx.Repository/Migrations/20241022080135_initial.cs
@@ -7,7 +7,7 @@
 namespace xxx.Repository.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -92,7 +92,18 @@ namespace xxx.Repository.Migrations
                 values: new object[,]
                 {
                     { 1, "Steven Spielberg" },
-                    { 2, "Christopher Nolan" }
+                    { 2, "Christopher Nolan" },
+                    { 3, "Martin Scorsese" },
+                    { 4, "Ridley Scott" },
+                    { 5, "Peter Jackson" },
+                    { 6, "Lana Wachowski" },
+                    { 7, "Justin Lin" },
+                    { 8, "Quentin Tarantino" },
+                    { 9, "James Cameron" },
+                    { 10, "Alex Kurtzman" },
+                    { 11, "Shawn Levy" },
+                    { 12, "Michael Bay" },
+                    { 13, "Roland Emmerich" }
                 });
 
             migrationBuilder.InsertData(
@@ -101,7 +112,18 @@ namespace xxx.Repository.Migrations
                 values: new object[,]
                 {
                     { 1, "Action" },
-                    { 2, "Drama" }
+                    { 2, "adventure" },
+                    { 3, "comedy" },
+                    { 4, "Drama" },
+                    { 5, "fantasy" },
+                    { 6, "horror" },
+                    { 7, "musicals" },
+                    { 8, "mystery" },
+                    { 9, "romance" },
+                    { 10, "sci-fi" },
+                    { 11, "sports" },
+                    { 12, "thriller" },
+                    { 13, "Western" }
                 });
 
             migrationBuilder.InsertData(
@@ -109,9 +131,19 @@ namespace xxx.Repository.Migrations
                 columns: new[] { "Id", "DirectorId", "DurationMinutes", "GenreId", "Title" },
                 values: new object[,]
                 {
-                    { 1, 1, 33, 1, "Fight With The Mongols" },
-                    { 2, 2, 33, 1, "Fight With The Small Mongols - The Movie" },
-                    { 3, 2, 33, 2, "Fight With The Big Mongols - The End" }
+                    { 1, 1, 127, 12, "Jurassic Park" },
+                    { 2, 2, 169, 10, "Interstellar" },
+                    { 3, 3, 126, 8, "Hugo" },
+                    { 4, 4, 144, 4, "The Martian" },
+                    { 5, 5, 201, 5, "The Lord of the Rings: The Return of the King" },
+                    { 6, 6, 136, 10, "The Matrix" },
+                    { 7, 7, 107, 12, "Fast & Furious" },
+                    { 8, 8, 111, 1, "Kill Bill: Vol. 1" },
+                    { 9, 9, 162, 5, "Avatar" },
+                    { 10, 10, 111, 6, "The Mummy" },
+                    { 11, 11, 108, 3, "Night at the Museum" },
+                    { 12, 12, 144, 1, "Transformers " },
+                    { 13, 13, 131, 12, "White House Down" }
                 });
 
             migrationBuilder.InsertData(
@@ -120,7 +152,18 @@ namespace xxx.Repository.Migrations
                 values: new object[,]
                 {
                     { 1, "Greate Movie", 1, 5 },
-                    { 2, "Not bad.", 2, 3 }
+                    { 2, "Not bad.", 2, 3 },
+                    { 3, "not gona watch again", 3, 1 },
+                    { 4, "awesome movie love the aactor that is left on mars", 4, 9 },
+                    { 5, "could be better", 5, 2 },
+                    { 6, "good movie", 6, 6 },
+                    { 7, "i am gona watch this with my familie next time", 7, 8 },
+                    { 8, "Love lovie this movie", 8, 7 },
+                    { 9, "awesome", 9, 6 },
+                    { 10, "good movie love it ", 10, 8 },
+                    { 11, "i am gona recommend to all i know", 11, 9 },
+                    { 12, "act better or i am gonna not gona watch you movies again", 12, 2 },
+                    { 13, "this is the worst movie ever", 13, 1 }
                 });
 
             migrationBuilder.CreateIndex(
